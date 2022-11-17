23548cfcc6e4d37fce78eb888d93ca24b3b9d08b
Casells <dmytro.syrotiak@gmail.com>
2022-11-18 00:50:32 +0200

    Add game over

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-let config = {
+const config = {
     map: {
         row: 20,
         col: 25,
@@ -7,6 +7,7 @@ let config = {
         }
     },
     snake: {
+        startTails: 3,
         size: 32,
         head: {
             color: "rgba(128,0,38,0.8)"
@@ -16,53 +17,63 @@ let config = {
         }
     },
     berry: {
+        maxBerry: 3,
         size: 16
     }
 }
+
 let pause = false;
+let start = false;
+
 let berry = {
-    position: {
-        x: 0,
-        y: 0
-    },
+    positions: [],
     check(x, y) {
-        if ( berry.position.x ===  x && berry.position.y ===  y) {
-            berry.spawn()
-            return true;
-        }
-        return false;
+        berry.positions.forEach((position, index) => {
+            if (position.x ===  x && position.y ===  y) {
+                scope.increment();
+                snake.maxTails ++
+                berry.positions.splice(index, 1)
+            }
+        })
     },
     spawn: () => {
-        berry.position.x = getRandomInt(0, config.map.col)
-        berry.position.y = getRandomInt(0, config.map.row)
+        if (berry.positions.length <= config.berry.maxBerry) {
+
+            berry.positions.push({
+                x: getRandomInt(0, config.map.col),
+                y: getRandomInt(0, config.map.row),
+                color: randomColor()
+            })
+        }
+
     },
     draw: () => {
-        context.beginPath();
-        context.fillStyle = "#283636";
-        const x = berry.position.x * config.map.cell.size + (config.map.cell.size / 2)
-        const y = berry.position.y * config.map.cell.size + (config.map.cell.size / 2)
-        context.arc(x, y, config.berry.size, 0, 2 * Math.PI );
-        context.fill();
+        berry.positions.forEach((position) => {
+            context.beginPath();
+            context.fillStyle = position.color;
+            const x = position.x * config.map.cell.size + (config.map.cell.size / 2)
+            const y = position.y * config.map.cell.size + (config.map.cell.size / 2)
+            context.arc(x, y, config.berry.size, 0, 2 * Math.PI );
+            context.fill();
+        })
     }
 }
 
 let snake = {
     forward: {
-        dx:0,
+        dx:1,
         dy:0
     },
     head: {
         x: 0,
         y: 10
     },
-    maxTails: 3,
+    maxTails: config.snake.startTails,
     tails: [],
     culcPosition: () => {
         snake.head.y += snake.forward.dy;
         snake.head.x += snake.forward.dx;
-        if (berry.check(snake.head.x, snake.head.y)) {
-            snake.maxTails ++
-        }
+        berry.check(snake.head.x, snake.head.y)
         snake.collisionBorder()
     },
     draw:() => {
@@ -71,7 +82,12 @@ let snake = {
             snake.tails.pop();
         }
         snake.tails.forEach( (el, index) => {
-            context.fillStyle = index? config.snake.tail.color : config.snake.head.color;
+            if(allowSidor) {
+                context.fillStyle = randomColor();
+            }
+            else {
+                context.fillStyle = index? config.snake.tail.color : config.snake.head.color;
+            }
             if (index === 0) {
                 context.beginPath();
                 const x = el.x * config.map.cell.size + (config.map.cell.size / 2)
@@ -81,19 +97,22 @@ let snake = {
             } else {
                 context.fillRect(el.x * config.map.cell.size, el.y * config.map.cell.size, config.map.cell.size, config.map.cell.size );
             }
-
-
-
+            snake.collisionTails(index)
         })
-
-
+    },
+    collisionTails: (index) => {
+        snake.tails.forEach( (el, j) => {
+            if((index !== j) && (el.x === snake.tails[index].x) &&  (el.y === snake.tails[index].y)) {
+                restartGame()
+            }
+        })
     },
     collisionBorder:() => {
-        if(snake.head.y > config.map.row) {
+        if(snake.head.y > config.map.row -1) {
             snake.head.y = 0
             return
         }
-        if(snake.head.x > config.map.col) {
+        if(snake.head.x > config.map.col - 1) {
             snake.head.x = 0
             return
         }
@@ -104,7 +123,28 @@ let snake = {
         if(snake.head.x < 0) {
             snake.head.x = config.map.col -1
         }
+    },
+    reset: () => {
+        snake.head.x = 0;
+        snake.head.y = 0;
+        snake.maxTails = config.snake.startTails;
+        snake.tails = [];
     }
+}
+
+let scopeBlock = document.getElementById("scope-number")
+
+const scope = {
+    scope: 0,
+    increment: () => {
+        scope.scope++
+        scopeBlock.innerHTML = scope.scope;
+    },
+    reset: () => {
+        scope.scope = 0;
+        scopeBlock.innerHTML = scope.scope;
+    }
+
 }
 
 const canvas = document.getElementById("canvas")
@@ -114,46 +154,79 @@ canvas.setAttribute("width", config.map.col * config.map.cell.size + "px")
 canvas.setAttribute("height", config.map.row * config.map.cell.size + "px")
 
 let context = canvas.getContext("2d");
-
-
 document.addEventListener("keydown", function (e) {
     if ( e.code === "KeyW" ) {
-        snake.forward.dy = -1;
+        snake.forward.dy = snake.forward.dy === 1?  snake.forward.dy:  -1;
         snake.forward.dx = 0;
         return;
     }
     if ( e.code === "KeyA" ) {
         snake.forward.dy = 0;
-        snake.forward.dx = -1;
+        snake.forward.dx = snake.forward.dx === 1?  snake.forward.dx:  -1;
         return;
     }
     if ( e.code === "KeyS" ) {
-        snake.forward.dy = 1;
+        snake.forward.dy = snake.forward.dy === -1?  snake.forward.dy:  1;
         snake.forward.dx = 0;
         return;
     }
     if ( e.code === "KeyD" ) {
         snake.forward.dy = 0;
-        snake.forward.dx = 1;
+        snake.forward.dx = snake.forward.dx === -1?  snake.forward.dx:  1;
         return;
     }
     if( e.code === "Space") {
         pause = !pause
     }
 });
-
-
+let allowSidor = false
 const getRandomInt = (min, max) => Math.floor( Math.random() * (max - min) + min )
-
-
-berry.spawn();
 const loop = () => {
-    if(pause) return
-
+    if(pause || (!start)) return
+    if(scope.scope === 20) {
+        startSidor()
+    }
     context.clearRect(0, 0, canvas.width, canvas.height);
     berry.draw();
     snake.culcPosition();
     snake.draw()
 }
 
-setInterval(loop, 120)
+setInterval(loop, 60)
+setInterval(berry.spawn, 5000)
+
+
+//Menu
+let menu = document.getElementById("menu")
+let startButton = document.getElementById("start-game")
+
+startButton.addEventListener("click", () => {
+    start = true
+    berry.spawn();
+    menuHide()
+})
+
+const menuHide = () => {
+    menu.style.display = "none"
+}
+const menuShow = () => {
+    menu.style.display = "block"
+}
+
+const restartGame = () => {
+    menuShow()
+    start = false
+    scope.reset();
+    snake.reset();
+}
+
+const randomColor = () => "#"+ Math.floor(Math.random()*16777215).toString(16);
+
+const startSidor = () => {
+    canvas.style.backgroundImage = "url('../assets/boomb.png')"
+    allowSidor = true;
+    setTimeout(() => {
+        canvas.style.backgroundImage = "url('../assets/Lacey Square SVG.png')"
+        allowSidor = false;
+    }, 8000)
+}
